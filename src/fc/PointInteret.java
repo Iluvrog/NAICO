b940f2e8a2aa978b4892104eb08c60c4fc52a689
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2022-11-07 20:56:27 +0100

    renommage masque

--- src/fc/PointInteret.java
+++ src/fc/PointInteret.java
@@ -4,35 +4,35 @@ import java.util.ArrayList;
 
 public class PointInteret {
 
-    public static double[][] getMap(int[][] image){
-        int hauteur = image.length;
-        int largeur = image[0].length;
+    public static double[][] getMap(int[][] masque){
+        int hauteur = masque.length;
+        int largeur = masque[0].length;
 
         double[][] map = new double[hauteur][largeur];
         for (int i = 0; i < hauteur; i++){
             for (int j = 0; j < largeur; j++){
-                map[i][j] = getContrasteVoisin(i, j, image);
+                map[i][j] = getContrasteVoisin(i, j, masque);
             }
         }
         return map;
     }
 
-    private static double getContrasteVoisin(int i, int j, int[][] image){
+    private static double getContrasteVoisin(int i, int j, int[][] masque){
         int VOISINS = 2;
         int SIGMA = 1;
 
         double contrasteVoisin = 0;
         for (int i2 = i- VOISINS; i2 <= i+ VOISINS; i2++){
             for (int j2 = j- VOISINS; j2 <= j+ VOISINS; j2++){
-                contrasteVoisin += getEcart(i, j, i2, j2, image) * Math.exp(-Math.sqrt((i-i2)*(i-i2)+(j-j2)*(j-j2))/ SIGMA);
+                contrasteVoisin += getEcart(i, j, i2, j2, masque) * Math.exp(-Math.sqrt((i-i2)*(i-i2)+(j-j2)*(j-j2))/ SIGMA);
             }
         }
         return contrasteVoisin;
     }
 
-    private static double getEcart(int i1, int j1, int i2, int j2, int[][] image){
-        if (i1<0 || j1<0 || i2<0 || j2<0 || i1>=image.length || i2>= image.length || j1>=image[0].length || j2>=image[0].length) return 0;
-        return Math.abs(image[i1][j1] - image[i2][j2]);
+    private static double getEcart(int i1, int j1, int i2, int j2, int[][] masque){
+        if (i1<0 || j1<0 || i2<0 || j2<0 || i1>=masque.length || i2>= masque.length || j1>=masque[0].length || j2>=masque[0].length) return 0;
+        return Math.abs(masque[i1][j1] - masque[i2][j2]);
     }
 
     public static ArrayList<int[]> getPointsInterets(double[][] map){
