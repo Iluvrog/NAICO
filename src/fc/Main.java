e0cd65f1def0c64820bfb92ccec647a53a021fef
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2022-11-21 22:56:12 +0100

    suppresion test inutile

--- src/fc/Main.java
+++ src/fc/Main.java
@@ -2,38 +2,7 @@ package fc;
 
 import fc.Comparateur.Comparateur;
 
-import java.util.ArrayList;
-
 public class Main {
-
-    //Teste les versions texte de Masque et Map sur un exemple
-    private void test1(){
-        int n = 5;
-        int[][] test = new int[n][n];
-        for (int i = 1; i < n-1; i++){
-            for (int j = 1; j < n-1; j++){
-                test[i][j] = 1;
-            }
-        }
-
-        Masque masque = new Masque(test);
-        System.out.println(masque);
-
-        Map map = new Map(masque);
-        System.out.println(map);
-
-        ArrayList<int[]> points = map.getPointsInterets();
-        for (int[] point : points){
-            System.out.println(point[0] + " " + point[1]);
-        }
-    }
-
-    //Je teste la fonction de comparaison des Map
-    private void test2(){
-        Map map1 = new Map(new Masque("./data/picture/A.png"));
-        Map map2 = new Map(new Masque("./data/picture/A.png"));
-        System.out.println(map1.compare(map2));
-    }
 
     //Idem
     private void test3(){
@@ -52,6 +21,6 @@ public class Main {
     public static void main(String[] args){
         Main main = new Main();
 
-        main.test1();
+        main.test4();
     }
 }
