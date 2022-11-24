24b6594ee4c8839f1c30cc7309f98a1aabf56884
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2022-11-24 18:26:34 +0100

    Add Verbose

---
+++ src/fc/Verbose/Verbose.java
@@ -0,0 +1,50 @@
+package fc.Verbose;
+
+import java.util.ArrayList;
+
+public class Verbose {
+
+    private char winner;
+    private double score;
+
+    private ArrayList<CelluleVerbose> celluleVerboses;
+
+    Verbose(){
+        winner = 0;
+        score = -1;
+        celluleVerboses = new ArrayList<>();
+    }
+
+    public void add(char candidat, double newscore, String type){
+        if (newscore > score){
+            winner = candidat;
+            score = newscore;
+        }
+
+        boolean haveCel = false;
+        for (CelluleVerbose cv : celluleVerboses){
+            if (cv.getVerboseString().startsWith(type)){
+                cv.add(candidat + " : " + newscore);
+                haveCel = true;
+            }
+        }
+
+        if (!haveCel){
+            CelluleVerbose newCellule = new CelluleVerbose();
+            newCellule.add(type + " :" + "\t" + candidat + " : " + newscore);
+            celluleVerboses.add(newCellule);
+        }
+    }
+
+    public char getWinner() {
+        return winner;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public ArrayList<CelluleVerbose> getCelluleVerboses() {
+        return celluleVerboses;
+    }
+}
