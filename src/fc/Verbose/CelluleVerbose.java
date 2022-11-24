b85d98ed9c765522248a6e5d856a82da5c7253ef
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2022-11-24 18:04:58 +0100

    Ajout celluleVerbose

---
+++ src/fc/Verbose/CelluleVerbose.java
@@ -0,0 +1,22 @@
+package fc.Verbose;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class CelluleVerbose {
+
+    private String verboseString;
+
+    CelluleVerbose(){
+        verboseString = "";
+    }
+
+    public String getVerboseString() {
+        return verboseString;
+    }
+
+    public void add(String add){
+        if (!Objects.equals(verboseString, "")) verboseString += "\t";
+        verboseString += add;
+    }
+}
